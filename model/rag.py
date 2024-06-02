46486b7a0f492dec77f14df7442ae3e4ec5eb0c6
mikoba0206 <zeroualihamza0206@gmail.com>
2024-06-02 03:50:48 +0200

    change chroma with FAISS

--- model/rag.py
+++ model/rag.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import UnstructuredMarkdownLoader
-from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
@@ -44,7 +44,7 @@ class Rag:
     
     def retrieve_document(self):
         splits = self.load_and_split_documents()
-        vectorstore = Chroma.from_documents(documents=splits, embedding=self.openai_embeddings)
+        vectorstore = FAISS.from_documents(documents=splits, embedding=self.openai_embeddings)
         return vectorstore.as_retriever()
 
     def make_rag_chain(self):
